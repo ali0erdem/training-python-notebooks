0a0fa17f89df8cd1a4db0e22423fe5ac1e34433c
Ali Erdem <aerdem.ie@gmail.com>
2020-05-02 01:42:14 +0300

    Exceptions module is completed.

---
+++ exceptional2.py
@@ -0,0 +1,28 @@
+DIGIT_MAP = {
+'zero' : '0',
+'one' : '1',
+'two' : '2',
+'three' : '3',
+'four' : '4',
+'five' : '5',
+'six' : '6',
+'seven' : '7',
+'eight' : '8',
+'nine' : '9'
+}
+
+def convert2(s):
+    try:
+        
+        number = ''
+        for token in s:
+            number += DIGIT_MAP[token]
+        x = int(number)
+        print(f"Conversion succeeded! x = {x}")
+    
+    except KeyError:
+        
+        print(f"Conversion failed!")
+        x = -1
+        
+    return x
